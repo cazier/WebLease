10c6d01f015263ad5f9b9b0901de48d1df359632
Brendan Cazier <admin@brendancazier.com>
2022-10-30 15:54:24 +0000

    Download testing

--- tests/loading/test_download.py
+++ tests/loading/test_download.py
@@ -1,10 +1,14 @@
 # type: ignore
 
 import pathlib
+import warnings
 from io import BytesIO
 from zipfile import ZipFile
 
+import httpx
+import respx
 from ward import Scope, test, raises, fixture
+from httpx import Response
 
 from tests.conftest import tmpdir
 from weblease.loading.download import get, extract
@@ -23,7 +27,18 @@ def _setup_teardown(path: pathlib.Path = tmpdir) -> pathlib.Path:
     yield
 
 
-@test("download: empty zip file", tags=["download"])
+@fixture(scope=Scope.Module)
+def mocking(files_path: pathlib.Path = tmpdir):
+    with respx.mock(base_url="https://example.com/") as mock:
+        mock.get("/empty.zip").mock(side_effect=httpx.RequestError)
+
+        file = mock.get("/not_empty.zip")
+        file.return_value = Response(200, content=files_path.joinpath("not_empty.zip").read_bytes())
+
+        yield
+
+
+@test("extract: empty zip file", tags=["download"])
 def _(files_path: pathlib.Path = tmpdir, _: None = _setup_teardown) -> None:
     with raises(FileNotFoundError) as exception:
         extract(BytesIO(files_path.joinpath("empty.zip").read_bytes()))
@@ -31,6 +46,28 @@ def _(files_path: pathlib.Path = tmpdir, _: None = _setup_teardown) -> None:
     assert str(exception.raised) == "Unable to find a useful file in the archive"
 
 
-@test("download: valid zip file", tags=["download"])
+@test("extract: valid zip file", tags=["download"])
 def _(files_path: pathlib.Path = tmpdir, _: None = _setup_teardown) -> None:
     assert extract(BytesIO(files_path.joinpath("not_empty.zip").read_bytes())) == "text"
+
+
+@test("decode: warning on malformed character", tags=["download"])
+def _() -> None:
+    with warnings.catch_warnings(record=True) as warn:
+        resp = chr(128).encode("utf8").decode("ascii", errors="replace_warning")
+
+    assert "The loaded file had a malformed character string" in str(warn[-1].message)
+    assert resp == "��"
+
+
+@test("download: from remote 404", tags=["download"])
+def _(_: respx.MockRouter = mocking) -> None:
+    with raises(httpx.RequestError):
+        get("https://example.com/empty.zip")
+
+
+@test("download: from remote", tags=["download"])
+def _(files_path: pathlib.Path = tmpdir, _: None = mocking, __: None = _setup_teardown) -> None:
+    response = get("https://example.com/not_empty.zip")
+
+    assert files_path.joinpath("not_empty.zip").read_bytes() == response.read()
--- weblease/loading/download.py
+++ weblease/loading/download.py
@@ -1,14 +1,29 @@
+import os
+import pathlib
 import zipfile
+import warnings
 from io import BytesIO
+from codecs import register_error, replace_errors
 
-import requests
+import httpx
+
+if os.getenv("WEBLEASE_MOCK_DOWNLOAD") == "1":
+
+    def handler(request: httpx.Request) -> httpx.Response:
+        if (file := pathlib.Path("cache").joinpath(request.url.path.split("/")[-1])).exists():
+            return httpx.Response(200, content=file.read_bytes())
+
+        return httpx.Response(400)
+
+    client = httpx.Client(transport=httpx.MockTransport(handler))
+
+else:
+    client = httpx.Client(transport=httpx.HTTPTransport())
 
 
 def get(url: str) -> BytesIO:
-    data = requests.get(url, timeout=30)
-
-    if not data.ok:
-        raise requests.RequestException(f"Could not download the data from {url}")
+    data = client.get(url, timeout=30)
+    data.raise_for_status()
 
     return BytesIO(data.content)
 
@@ -17,6 +32,14 @@ def extract(zip_data: BytesIO) -> str:
     with zipfile.ZipFile(zip_data) as zip_file:
         for file in zip_file.filelist:
             if file.file_size > 0:
-                return zip_file.read(file).decode("ascii")
+                return zip_file.read(file).decode("ascii", errors="replace_warning")
 
     raise FileNotFoundError("Unable to find a useful file in the archive")
+
+
+def replace_warning(exception: UnicodeError) -> tuple[str | bytes, int]:
+    warnings.warn('The loaded file had a malformed character string which was replaced by a "?"')
+    return replace_errors(exception)
+
+
+register_error("replace_warning", replace_warning)
