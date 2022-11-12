1ad65835eb3942643466ced210ae8fb36de93aff
Brendan Cazier <admin@brendancazier.com>
2022-11-12 00:18:17 +0000

    Splitting out parse date

--- weblease/models/field_types.py
+++ weblease/models/field_types.py
@@ -4,13 +4,13 @@ import typing as t
 from tortoise import fields
 from tortoise.models import Model
 
-DATE_FORMATS = ("%m/%d/%Y", "%Y%m%d", "%Y-%m-%d")
+from weblease.models.utils import parse_date
 
 
 class BseeDateField(fields.DateField):
     def to_python_value(self, value: t.Any) -> t.Optional[dt.date]:
         if value is not None and not isinstance(value, dt.date) and value != "":
-            out = BseeDateField.parse_date(value)
+            out = parse_date(value)
 
         else:
             out = None
@@ -22,20 +22,9 @@ class BseeDateField(fields.DateField):
         self, value: t.Optional[dt.date | str], instance: Model | t.Type[Model]
     ) -> t.Optional[dt.date]:
         if isinstance(value, str):
-            return super().to_db_value(BseeDateField.parse_date(value), instance)
+            return super().to_db_value(parse_date(value), instance)
 
         return super().to_db_value(value, instance)
-
-    @staticmethod
-    def parse_date(value: str) -> dt.date:
-        for fmt in DATE_FORMATS:
-            try:
-                return dt.datetime.strptime(value, fmt).date()
-
-            except ValueError:
-                pass
-
-        raise ValueError(f"Could not find parse a date value from: {value}")
 
 
 class BaseCompany:
--- weblease/models/utils.py
+++ weblease/models/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import typing as t
 
 
@@ -35,3 +36,34 @@ class FieldFinder:
             dict[str, str]: the key-value pairs of the model data
         """
         return {key: data.pop(key) for key in cls.fields()}
+
+
+DATE_FORMATS = ("%m/%d/%Y", "%Y%m%d", "%Y-%m-%d")
+
+
+def parse_date(value: t.Optional[str]) -> t.Optional[datetime.date]:
+    """Parse a date string into its corresponding :py:class:`datetime.date` object by iterating over
+    a list of possible formats.
+
+    The list can be found at :py:attr:`DATE_FORMATS`
+
+    Args:
+        value (t.Optional[str]): possible date string
+
+    Raises:
+        ValueError: Raised when the string cannot be parsed into a datetime object
+
+    Returns:
+        t.Optional[datetime.date]: the parsed object
+    """
+    if value is None:
+        return None
+
+    for fmt in DATE_FORMATS:
+        try:
+            return datetime.datetime.strptime(value, fmt).date()
+
+        except ValueError:
+            pass
+
+    raise ValueError(f"Could not find parse a date value from: {value}")
